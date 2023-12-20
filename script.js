7d2d17998e7a631a6575c4ffac3e51dd8fa72b9e
megha77777 <141833704+megha77777@users.noreply.github.com>
2023-12-20 15:57:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
 // your code here
+	// let num = prompt("Enter a number")
+	let num = Number.parseInt(num);
+	let a=-1, b=1;
+	for(let i=1; i<=num; i++){
+		let c = a+b;
+		b=c;
+		a=b;
+	}
+	return c;
 }
-
 module.exports = fibonacci;
